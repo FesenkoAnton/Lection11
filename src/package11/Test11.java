35b9f44bf16d1271e39eb1bb7239062f3abee0ff
FesenkoAnton <antonfesenkouk@gmail.com>
2016-04-08 20:56:10 +0300

    Git commit Lection11

---
+++ src/package11/Test11.java
@@ -0,0 +1,21 @@
+package package11;
+
+import acm.graphics.*; 
+import acm.program.*; 
+import acm.util.*; 
+
+import java.awt.*; 
+import java.awt.event.*; 
+
+/** This class displays a mouse-draggable rectangle and oval */ 
+public class Test11 extends GraphicsProgram { 
+	
+	// Initializes the program 
+	public void run() { 
+		int x;
+		for(int i=0;i<3;i++){
+			
+		
+		}
+	
+	}}
